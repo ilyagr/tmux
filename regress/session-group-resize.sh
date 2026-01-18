ilyagr/tmux

commit 2bb0b9d6c5edd7c4127c971f5ccebed969f86c1c

Change focus to be driven by events rather than walking all panes at end of event loop, this way the ordering of in and out can be enforced. GitHub issue 2808.

server-client.c
@@ -33,7 +33,6 @@
 #include "tmux.h"
 
 static void	server_client_free(int, short, void *);
-static void	server_client_check_pane_focus(struct window_pane *);
 static void	server_client_check_pane_resize(struct window_pane *);
 static void	server_client_check_pane_buffer(struct window_pane *);
 static void	server_client_check_window_resize(struct window *);
@@ -301,14 +300,47 @@ server_client_attached_lost(struct client *c)
 			s = loop->session;
 			if (loop == c || s == NULL || s->curw->window != w)
 				continue;
-			if (found == NULL ||
-			    timercmp(&loop->activity_time, &found->activity_time,
-			    >))
+			if (found == NULL || timercmp(&loop->activity_time,
+			    &found->activity_time, >))
 				found = loop;
 		}
 		if (found != NULL)
 			server_client_update_latest(found);
 	}
+}
+
+
+/* Set client session. */
+void
+server_client_set_session(struct client *c, struct session *s)
+{
+	struct session	*old = c->session;
+
+	if (s != NULL && c->session != NULL && c->session != s)
+		c->last_session = c->session;
+	else if (s == NULL)
+		c->last_session = NULL;
+	c->session = s;
+	c->flags |= CLIENT_FOCUSED;
+	recalculate_sizes();
+
+	if (old != NULL && old->curw != NULL)
+		window_update_focus(old->curw->window);
+	if (s != NULL) {
+		window_update_focus(s->curw->window);
+		session_update_activity(s, NULL);
+		gettimeofday(&s->last_attached_time, NULL);
+		s->curw->flags &= ~WINLINK_ALERTFLAGS;
+		s->curw->window->latest = c;
+		alerts_check_session(s);
+		tty_update_client_offset(c);
+		status_timer_start(c);
+		notify_client("client-session-changed", c);
+		server_redraw_client(c);
+	}
+
+	server_check_unattached();
+	server_update_socket();
 }
 
 /* Lost a client. */
@@ -1389,7 +1421,6 @@ server_client_loop(void)
 	struct client		*c;
 	struct window		*w;
 	struct window_pane	*wp;
-	int			 focus;
 
 	/* Check for window resize. This is done before redrawing. */
 	RB_FOREACH(w, windows, &windows)
@@ -1407,14 +1438,11 @@ server_client_loop(void)
 
 	/*
 	 * Any windows will have been redrawn as part of clients, so clear
-	 * their flags now. Also check pane focus and resize.
+	 * their flags now.
 	 */
-	focus = options_get_number(global_options, "focus-events");
 	RB_FOREACH(w, windows, &windows) {
 		TAILQ_FOREACH(wp, &w->panes, entry) {
 			if (wp->fd != -1) {
-				if (focus)
-					server_client_check_pane_focus(wp);
 				server_client_check_pane_resize(wp);
 				server_client_check_pane_buffer(wp);
 			}
@@ -1613,54 +1641,6 @@ out:
 		bufferevent_disable(wp->event, EV_READ);
 	else
 		bufferevent_enable(wp->event, EV_READ);
-}
-
-/* Check whether pane should be focused. */
-static void
-server_client_check_pane_focus(struct window_pane *wp)
-{
-	struct client	*c;
-	int		 push;
-
-	/* Do we need to push the focus state? */
-	push = wp->flags & PANE_FOCUSPUSH;
-	wp->flags &= ~PANE_FOCUSPUSH;
-
-	/* If we're not the active pane in our window, we're not focused. */
-	if (wp->window->active != wp)
-		goto not_focused;
-
-	/*
-	 * If our window is the current window in any focused clients with an
-	 * attached session, we're focused.
-	 */
-	TAILQ_FOREACH(c, &clients, entry) {
-		if (c->session == NULL || !(c->flags & CLIENT_FOCUSED))
-			continue;
-		if (c->session->attached == 0)
-			continue;
-
-		if (c->session->curw->window == wp->window)
-			goto focused;
-	}
-
-not_focused:
-	if (push || (wp->flags & PANE_FOCUSED)) {
-		if (wp->base.mode & MODE_FOCUSON)
-			bufferevent_write(wp->event, "\033[O", 3);
-		notify_pane("pane-focus-out", wp);
-	}
-	wp->flags &= ~PANE_FOCUSED;
-	return;
-
-focused:
-	if (push || !(wp->flags & PANE_FOCUSED)) {
-		if (wp->base.mode & MODE_FOCUSON)
-			bufferevent_write(wp->event, "\033[I", 3);
-		notify_pane("pane-focus-in", wp);
-		session_update_activity(c->session, NULL);
-	}
-	wp->flags |= PANE_FOCUSED;
 }
 
 /*
@@ -2078,7 +2058,7 @@ server_client_dispatch(struct imsg *imsg, void *arg)
 	case MSG_EXITING:
 		if (datalen != 0)
 			fatalx("bad MSG_EXITING size");
-		c->session = NULL;
+		server_client_set_session(c, NULL);
 		tty_close(&c->tty);
 		proc_send(c->peer, MSG_EXITED, -1, NULL, 0);
 		break;

cmd-attach-session.c
@@ -124,17 +124,9 @@ cmd_attach_session(struct cmdq_item *item, const char *tflag, int dflag,
 		if (!Eflag)
 			environ_update(s->options, c->environ, s->environ);
 
-		c->session = s;
+		server_client_set_session(c, s);
 		if (~cmdq_get_flags(item) & CMDQ_STATE_REPEAT)
 			server_client_set_key_table(c, NULL);
-		tty_update_client_offset(c);
-		status_timer_start(c);
-		notify_client("client-session-changed", c);
-		session_update_activity(s, NULL);
-		gettimeofday(&s->last_attached_time, NULL);
-		server_redraw_client(c);
-		s->curw->flags &= ~WINLINK_ALERTFLAGS;
-		s->curw->window->latest = c;
 	} else {
 		if (server_client_open(c, &cause) != 0) {
 			cmdq_error(item, "open terminal failed: %s", cause);
@@ -156,25 +148,14 @@ cmd_attach_session(struct cmdq_item *item, const char *tflag, int dflag,
 		if (!Eflag)
 			environ_update(s->options, c->environ, s->environ);
 
-		c->session = s;
+		server_client_set_session(c, s);
 		server_client_set_key_table(c, NULL);
-		tty_update_client_offset(c);
-		status_timer_start(c);
-		notify_client("client-session-changed", c);
-		session_update_activity(s, NULL);
-		gettimeofday(&s->last_attached_time, NULL);
-		server_redraw_client(c);
-		s->curw->flags &= ~WINLINK_ALERTFLAGS;
-		s->curw->window->latest = c;
 
 		if (~c->flags & CLIENT_CONTROL)
 			proc_send(c->peer, MSG_READY, -1, NULL, 0);
 		notify_client("client-attached", c);
 		c->flags |= CLIENT_ATTACHED;
 	}
-	recalculate_sizes();
-	alerts_check_session(s);
-	server_update_socket();
 
 	return (CMD_RETURN_NORMAL);
 }

cmd-new-session.c
@@ -329,18 +329,10 @@ cmd_new_session_exec(struct cmd *self, struct cmdq_item *item)
 				proc_send(c->peer, MSG_READY, -1, NULL, 0);
 		} else if (c->session != NULL)
 			c->last_session = c->session;
-		c->session = s;
+		server_client_set_session(c, s);
 		if (~cmdq_get_flags(item) & CMDQ_STATE_REPEAT)
 			server_client_set_key_table(c, NULL);
-		tty_update_client_offset(c);
-		status_timer_start(c);
-		notify_client("client-session-changed", c);
-		session_update_activity(s, NULL);
-		gettimeofday(&s->last_attached_time, NULL);
-		server_redraw_client(c);
 	}
-	recalculate_sizes();
-	server_update_socket();
 
 	/*
 	 * If there are still configuration file errors to display, put the new

cmd-switch-client.c
@@ -134,23 +134,9 @@ cmd_switch_client_exec(struct cmd *self, struct cmdq_item *item)
 	if (!args_has(args, 'E'))
 		environ_update(s->options, tc->environ, s->environ);
 
-	if (tc->session != NULL && tc->session != s)
-		tc->last_session = tc->session;
-	tc->session = s;
+	server_client_set_session(tc, s);
 	if (~cmdq_get_flags(item) & CMDQ_STATE_REPEAT)
 		server_client_set_key_table(tc, NULL);
-	tty_update_client_offset(tc);
-	status_timer_start(tc);
-	notify_client("client-session-changed", tc);
-	session_update_activity(s, NULL);
-	gettimeofday(&s->last_attached_time, NULL);
-
-	server_check_unattached();
-	server_redraw_client(tc);
-	s->curw->flags &= ~WINLINK_ALERTFLAGS;
-	s->curw->window->latest = tc;
-	recalculate_sizes();
-	alerts_check_session(s);
 
 	return (CMD_RETURN_NORMAL);
 }

input.c
@@ -1792,8 +1792,12 @@ input_csi_dispatch_sm_private(struct input_ctx *ictx)
 			if (sctx->s->mode & MODE_FOCUSON)
 				break;
 			screen_write_mode_set(sctx, MODE_FOCUSON);
-			if (wp != NULL)
-				wp->flags |= PANE_FOCUSPUSH; /* force update */
+			if (wp == NULL)
+				break;
+			if (wp->flags & PANE_FOCUSED)
+				bufferevent_write(wp->event, "\033[I", 3);
+			else
+				bufferevent_write(wp->event, "\033[O", 3);
 			break;
 		case 1005:
 			screen_write_mode_set(sctx, MODE_MOUSE_UTF8);

server-fn.c
@@ -445,21 +445,9 @@ server_destroy_session(struct session *s)
 	TAILQ_FOREACH(c, &clients, entry) {
 		if (c->session != s)
 			continue;
-		if (s_new == NULL) {
-			c->session = NULL;
+		server_client_set_session(c, NULL);
+		if (s_new == NULL)
 			c->flags |= CLIENT_EXIT;
-		} else {
-			c->last_session = NULL;
-			c->session = s_new;
-			server_client_set_key_table(c, NULL);
-			tty_update_client_offset(c);
-			status_timer_start(c);
-			notify_client("client-session-changed", c);
-			session_update_activity(s_new, NULL);
-			gettimeofday(&s_new->last_attached_time, NULL);
-			server_redraw_client(c);
-			alerts_check_session(s_new);
-		}
 	}
 	recalculate_sizes();
 }

session.c
@@ -489,6 +489,8 @@ session_last(struct session *s)
 int
 session_set_current(struct session *s, struct winlink *wl)
 {
+	struct winlink	*old = s->curw;
+
 	if (wl == NULL)
 		return (-1);
 	if (wl == s->curw)
@@ -497,6 +499,10 @@ session_set_current(struct session *s, struct winlink *wl)
 	winlink_stack_remove(&s->lastw, wl);
 	winlink_stack_push(&s->lastw, s->curw);
 	s->curw = wl;
+	if (options_get_number(global_options, "focus-events")) {
+		window_update_focus(old->window);
+		window_update_focus(wl->window);
+	}
 	winlink_clear_flags(wl);
 	window_update_activity(wl->window);
 	tty_update_window_offset(wl->window);

tmux.h
@@ -1004,7 +1004,7 @@ struct window_pane {
 #define PANE_FOCUSED 0x4
 /* 0x8 unused */
 /* 0x10 unused */
-#define PANE_FOCUSPUSH 0x20
+/* 0x20 unused */
 #define PANE_INPUTOFF 0x40
 #define PANE_CHANGED 0x80
 #define PANE_EXITED 0x100
@@ -2506,6 +2506,7 @@ int	 server_client_handle_key(struct client *, struct key_event *);
 struct client *server_client_create(int);
 int	 server_client_open(struct client *, char **);
 void	 server_client_unref(struct client *);
+void	 server_client_set_session(struct client *, struct session *);
 void	 server_client_lost(struct client *);
 void	 server_client_suspend(struct client *);
 void	 server_client_detach(struct client *, enum msgtype);
@@ -2826,6 +2827,8 @@ struct window_pane *window_find_string(struct window *, const char *);
 int		 window_has_pane(struct window *, struct window_pane *);
 int		 window_set_active_pane(struct window *, struct window_pane *,
 		     int);
+void		 window_update_focus(struct window *);
+void		 window_pane_update_focus(struct window_pane *);
 void		 window_redraw_active_switch(struct window *,
 		     struct window_pane *);
 struct window_pane *window_add_pane(struct window *, struct window_pane *,

commit de7a1f2cb4890d14e2fd6f88e0e1cee6c726c2de

Fix window-size=latest not resizing on switch-client in session groups
When using session groups with window-size=latest (the default), switching
to a non-current window from a larger terminal did not resize the window
immediately. The resize only happened later when switching windows again.

The bug was in server_client_set_session(): recalculate_sizes() was called
BEFORE setting s->curw->window->latest to the current client. This caused
the resize calculation to use the old latest pointer (the smaller client)
instead of the new client that initiated the switch.

The fix moves the latest assignment before recalculate_sizes() so the
resize calculation uses the correct client. This matches how
cmd_select_window_exec() already handles this - it sets window->latest
before calling recalculate_sizes().

This bug was introduced in commit 2bb0b9d6 (August 2021) when the code
was refactored into server_client_set_session(). Before that refactor,
cmd-switch-client.c had the correct order: latest assignment first,
then recalculate_sizes().

Add a regression test that creates a session group with two control clients
of different sizes and verifies that switching windows resizes immediately.

server-client.c
@@ -403,13 +403,13 @@ server_client_set_session(struct client *c, struct session *s)
 	if (old != NULL && old->curw != NULL)
 		window_update_focus(old->curw->window);
 	if (s != NULL) {
+		s->curw->window->latest = c;
 		recalculate_sizes();
 		window_update_focus(s->curw->window);
 		session_update_activity(s, NULL);
 		session_theme_changed(s);
 		gettimeofday(&s->last_attached_time, NULL);
 		s->curw->flags &= ~WINLINK_ALERTFLAGS;
-		s->curw->window->latest = c;
 		alerts_check_session(s);
 		tty_update_client_offset(c);
 		status_timer_start(c);

regress/session-group-resize.sh
@@ -1,0 +1,58 @@
+#!/bin/sh
+
+# Test that window-size=latest works correctly when switching windows in
+# session groups. When a larger client switches to a window that was previously
+# only viewed by a smaller client, the window should resize immediately.
+#
+# This is a regression test for a bug where server_client_set_session() called
+# recalculate_sizes() before setting window->latest, causing the resize
+# calculation to use the old (smaller) client's size.
+
+PATH=/bin:/usr/bin
+TERM=screen
+
+[ -z "$TEST_TMUX" ] && TEST_TMUX=$(readlink -f ../tmux)
+TMUX="$TEST_TMUX -Ltest"
+$TMUX kill-server 2>/dev/null
+
+TMP1=$(mktemp)
+TMP2=$(mktemp)
+OUT=$(mktemp)
+trap "rm -f $TMP1 $TMP2 $OUT" 0 1 15
+
+# Create a session with two windows. After neww, current window is 1.
+$TMUX -f/dev/null new -d -s test -x 80 -y 24 || exit 1
+$TMUX neww -t test || exit 1
+sleep 0.5
+
+# Attach a small control client (80x24) to window 1 (the current window).
+(echo "refresh-client -C 80,24"; sleep 10) | $TMUX -f/dev/null -C a -t test >$TMP1 2>&1 &
+PID1=$!
+sleep 0.5
+
+# Create a grouped session with a larger control client (134x51).
+# Select window 0 so the large client views window 0 while small client views window 1.
+# Then switch to window 1 - this is where the bug manifests.
+(echo "refresh-client -C 134,51"
+ echo "selectw -t :0"
+ sleep 1
+ echo "switch-client -t :=1"
+ sleep 2) | $TMUX -f/dev/null -C new -t test -x 134 -y 51 >$TMP2 2>&1 &
+PID2=$!
+sleep 2.5
+
+# Check window 1's size - it should have resized to 134x51 immediately
+# when the larger client switched to it.
+$TMUX lsw -t test -F '#{window_index}:#{window_width}x#{window_height}' > $OUT
+
+# Clean up
+kill $PID1 $PID2 2>/dev/null
+wait $PID1 $PID2 2>/dev/null
+$TMUX kill-server 2>/dev/null
+
+# Window 1 should be 134x51 (the larger client's size).
+# Before the fix, window 1 stayed at 80x24 because recalculate_sizes()
+# was called before window->latest was updated.
+grep -q "1:134x51" $OUT || exit 1
+
+exit 0